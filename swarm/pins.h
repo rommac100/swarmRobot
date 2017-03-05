859cb1a5b324ef96944fb8a422ee10879fda6167
Sam Smith <rommac100@gmail.com>
2017-03-05 09:02:11 -0700

    pins.h file added for motors

---
+++ swarm/pins.h
@@ -0,0 +1,15 @@
+#ifndef pins.h
+#define pins.h
+//Pins file for everything
+
+//Pins for Stepper Motors
+#define lStepP0 5;
+#define lStepP1 6;
+#define lStepP2 7;
+#define lStepP3 8;
+#define rStepP0 0;
+#define rStepP1 1;
+#define rStepP2 2;
+#define rStepP3 3;
+
+//Pins for ultrasonic Sensor
