bf797b8ac18e90d421523128b6f57a1f0b0b6659
Sam Smith <rommac100@gmail.com>
2017-03-04 18:39:56 -0700

    new pins

--- swarm/swarm.ino
+++ swarm/swarm.ino
@@ -1,33 +1,39 @@
 #include <Stepper.h>
-int defaultStepsPerMil = 4096;
-Stepper leftStepper(4096, 5, 6, 7, 8);
-Stepper rightStepper(4096, 0, 1, 2, 3);
-//    #define  TIMEOUT     1000    //Turns off after 1 sec of inactivity.
-//    void CheckTimeout(){
-//      if((millis() - stampX) > TIMEOUT){   //Turn Off StepperX coils.
-//        digitalWrite(COIL_1X, LOW);
-//        digitalWrite(COIL_2X, LOW);
-//        digitalWrite(COIL_3X, LOW);
-//        digitalWrite(COIL_4X, LOW);
-//      }
-//      if((millis() - stampY) > TIMEOUT){   //Turn Off StepperY coils.
-//        digitalWrite(COIL_1Y, LOW);
-//        digitalWrite(COIL_2Y, LOW);
-//        digitalWrite(COIL_3Y, LOW);
-//        digitalWrite(COIL_4Y, LOW);
-//      }
-//    }//As per ardunaut on arduining.com https://arduining.com/2015/05/30/arduino-and-two-stepper-motors/
+#define lStepP0=5;
+#define lStepP1=6;
+#define lStepP2=7;
+#define lStepP3=8;
+#define rStepP0=0;
+#define rStepP1=1;
+#define rStepP2=2;
+#define rStepP3=3;
+Stepper leftStepper(4096, lStepP0, lStepP1, lStepP2, lStepP3);
+Stepper rightStepper(4096, rStepP0, rStepP1, rStepP2, rStepP3);
+#define  TIMEOUT     4000    //Turns off steppers after 4 sec of inactivity.
+void CheckTimeout(){
+  if((millis() - timeStamp) > TIMEOUT){   //Turn Off StepperX coils.
+    digitalWrite(lStepP0, LOW);
+    digitalWrite(lStepP1, LOW);
+    digitalWrite(lStepP2, LOW);
+    digitalWrite(lStepP3, LOW);
+    digitalWrite(rStepP0, LOW);
+    digitalWrite(rStepP1, LOW);
+    digitalWrite(rStepP2, LOW);
+    digitalWrite(rStepP3, LOW);
+  }
+}//As per ardunaut on arduining.com https://arduining.com/2015/05/30/arduino-and-two-stepper-motors/
 double[] moveForward(double runDist, double netAngle)
 {
   yPos = runDist*Math.cos(netAngle);
   xPos = runDist*Math.sin(netAngle);
   double stepDist = runDist/2;
   int stepCount = 0;
-  while(stepCount < stepDist)
+  while(stepCount < (int)stepDist)
   {
     leftStepper.step(1);
     rightStepper.step(1);
   }
+  timeStamp = millis()
   return [xPos,yPos];
 }
 double moveTurn(double angleTurn, double netAngle)
@@ -35,23 +41,12 @@ double moveTurn(double angleTurn, double netAngle)
   postNetAngle = netAngle+angleTurn;
   double runDist = 2.25*angleTurn;
   double stepDist = runDist/2;
-  //run rightStepper at convRunDist;
-  //run left Stepper at -convRunDist;
+  int stepCount = 0;
+  while(stepCount<(int)stepDist)
+  {
+    leftStepper.step(-1);
+    rightStepper.step(1);
+  }
+  timeStamp = millis()
   return postNetAngle;
-}
-
-void calibration()
-{
-  double[] new = moveForward(1,0);
-
-}
-
-void setup()
-{
-
-}
-
-void loop()
-{
-
 }
