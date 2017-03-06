94d62fe295470034c92f333b4dfd025145e62acd
Sam Smith <rommac100@gmail.com>
2017-03-05 21:15:59 -0700

    robot kind of works - ultra + moveDir completed

---
+++ stepper/src/pins.h
@@ -0,0 +1,23 @@
+#ifndef pins
+#define pins
+//Pins file for everything
+
+//Pins for Stepper Motors
+#define lStepP0 5
+#define lStepP1 4
+#define lStepP2 0
+#define lStepP3 2
+#define rStepP0 14
+#define rStepP1 12
+#define rStepP2 13
+#define rStepP3 15
+
+
+//Pins for ultrasonic Sensor
+#define ultra1PinIN 3
+#define ultra12PinOut 1
+
+
+
+
+#endif
---
+++ stepper/src/stepper.ino
@@ -0,0 +1,83 @@
+#include <Arduino.h>
+#include "pins.h"
+#include "StepperMotor.h"
+
+StepperMotor motorLeft(5,4,0,2);
+StepperMotor motorRight(14,12,13,15);
+
+const static int stepsPerInches = 410;
+
+long ultraSonicGetVal()
+{
+   long duration, inches;
+
+   //turning on Sensor
+   pinMode(ultra12PinOut, OUTPUT);
+   digitalWrite(ultra12PinOut, LOW);
+   delayMicroseconds(2);
+   digitalWrite(ultra12PinOut, HIGH);
+   delayMicroseconds(10);
+   digitalWrite(ultra12PinOut, LOW);
+
+   pinMode(ultra1PinIN, INPUT);
+   duration = pulseIn(ultra1PinIN, HIGH);
+
+   inches = duration/74/2;
+
+   return inches;
+}
+
+void moveDir(double inches)
+{
+  if (inches > 0)
+  {
+       int steps = 0;
+       while (steps < inches*stepsPerInches)
+       {
+       motorLeft.step(-10);
+       motorRight.step(10);
+       steps += 10;
+     }
+  }
+  else if (inches < 0)
+  {
+    int steps = 0;
+    while (steps < inches*stepsPerInches)
+    {
+       motorLeft.step(10);
+       motorRight.step(-10);
+    }
+  }
+
+}
+
+void setup(){
+  Serial.begin(9600);
+  motorLeft.setStepDuration(3);
+  motorRight.setStepDuration(3);
+}
+
+void loop(){
+moveDir(5);
+delay(10000);
+/*
+while (ultraSonicGetVal() > 22)
+  {
+    motorLeft.step(-10);
+    motorRight.step(10);
+    /*
+  int stepCount = 0;
+  while (stepCount<4096)
+  {
+    motorLeft.step(10);
+    motorRight.step(10);
+    stepCount+= 10;
+  }
+  delay(1000);
+  //}
+  */
+//}
+
+  //delay(5000);
+
+}
